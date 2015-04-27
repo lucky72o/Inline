3ca1ea69979eb48e882f67236515efcd47db2700
slava <gurmanov.slava@gmail.com>
2015-04-27 23:07:20 +0100

    added CharacterEncodingFilter + updated DataSource

--- src/config/org/inline/config/SpringWebAppInitializer.java
+++ src/config/org/inline/config/SpringWebAppInitializer.java
@@ -4,7 +4,10 @@ import org.inline.config.root.DevelopmentConfiguration;
 import org.inline.config.root.RootContextConfig;
 import org.inline.config.root.TestConfiguration;
 import org.inline.config.servlet.WebMvcContextConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class SpringWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -15,11 +18,21 @@ public class SpringWebAppInitializer extends AbstractAnnotationConfigDispatcherS
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{RootContextConfig.class, DevelopmentConfiguration.class};
+        return new Class<?>[]{RootContextConfig.class, DevelopmentConfiguration.class, TestConfiguration.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{ WebMvcContextConfig.class };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return new Filter[] { characterEncodingFilter};
     }
 }
--- src/config/org/inline/config/root/DevelopmentConfiguration.java
+++ src/config/org/inline/config/root/DevelopmentConfiguration.java
@@ -1,6 +1,5 @@
 package org.inline.config.root;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.inline.init.TestDataInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -43,7 +43,7 @@ public class DevelopmentConfiguration {
     //TODO: change datasource implementation
     @Bean(name = "dataSource")
     public DataSource getDataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
         dataSource.setUrl(env.getProperty("jdbc.url"));
         dataSource.setUsername(env.getProperty("jdbc.user"));
